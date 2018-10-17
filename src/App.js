c4cc88334236bfc56c5ed47b45d9eacf4bad6b8f
Greg Czaplicki <glc3344@gmail.com>
2018-10-17 13:54:22 -0400

    feature: basic logic and styling

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.newItem = React.createRef();
-  }
   state = {
-    items: []
-  };
-
-  addItem = () => {
-    const { items } = this.state;
-    const newItem = this.newItem.current.value;
-    this.setState({
-      items: [...items, newItem]
-    });
-    this.newItem.current.value = "";
+    items: {
+      Produce: [
+        { id: 1, itemName: "Parsley", itemQuantity: 1 },
+        { id: 2, itemName: "Basil", itemQuantity: 2 }
+      ],
+      Deli: [],
+      Snacks: [{ id: 1, itemName: "Chips", itemQuantity: 2 }]
+    }
   };
 
   render() {
     return (
       <div className="container">
         <h1>Grocery List</h1>
-        <input
-          ref={this.newItem}
-          placeholder="Add Item..."
-          type="text"
-          name="itemName"
-          id="itemInput"
-        />
-        <button onClick={this.addItem}>Add Item</button>
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlInput1">Item Name:</label>
+            <input
+              type="text"
+              className="form-control"
+              id="itemName"
+              placeholder="Add an item..."
+              autoFocus
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlSelect1">Category:</label>
+            <select className="form-control" id="itemCategory">
+              {Object.keys(this.state.items).map(category => (
+                <option key={category}>{category}</option>
+              ))}
+            </select>
+          </div>
+          <button type="submit" className="btn btn-primary btn-lg">
+            Add Item
+          </button>
+        </form>
         <h3>List:</h3>
-        <ul>
-          {this.state.items.map(item => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
+        {Object.keys(this.state.items).map(category => (
+          <div key={category}>
+            {this.state.items[category].length > 0 && <h4>{category}</h4>}
+            {this.state.items[category].map(item => (
+              <li key={item.id}>{item.itemName}</li>
+            ))}
+          </div>
+        ))}
       </div>
     );
   }
